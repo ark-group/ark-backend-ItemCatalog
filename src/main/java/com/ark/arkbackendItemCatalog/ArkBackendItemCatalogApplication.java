4b52d38d436dfa47d1e7e3edc6fba821c7cd2ff7
krrathore1990 <karanr15.rathore@gmail.com>
2019-05-04 01:18:50 +0530

    first commit

---
+++ src/main/java/com/ark/arkbackendItemCatalog/ArkBackendItemCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackendItemCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendItemCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendItemCatalogApplication.class, args);
+	}
+
+}
